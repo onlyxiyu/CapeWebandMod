2fa951181b7ac72d7b4c17f679a492fe1ea19539
饩雨 <2546380036@qq.com>
2025-02-13 20:48:03 +0800

    up

--- capemanages1.20.6/src/main/java/org/xiyu/yee/capemanages/mixin/PlayerMixin.java
+++ capemanages1.20.6/src/main/java/org/xiyu/yee/capemanages/mixin/PlayerMixin.java
@@ -1,22 +1,30 @@
-package org.xiyu.yee.capemanages.mixin;
+ package org.xiyu.yee.capemanages.mixin;
+ import net.minecraft.client.multiplayer.PlayerInfo;
+ import net.minecraft.client.player.AbstractClientPlayer;
+ import net.minecraft.client.resources.PlayerSkin;
+ import net.minecraft.resources.ResourceLocation;
+ import org.spongepowered.asm.mixin.Mixin;
+ import org.spongepowered.asm.mixin.Shadow;
+ import org.spongepowered.asm.mixin.injection.At;
+ import org.spongepowered.asm.mixin.injection.Inject;
+ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+ import org.xiyu.yee.capemanages.cape.CapeManager;
 
-import net.minecraft.client.player.AbstractClientPlayer;
-import net.minecraft.resources.ResourceLocation;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.xiyu.yee.capemanages.cape.CapeManager;
+ import javax.annotation.Nullable;
 
-@Mixin(AbstractClientPlayer.class)
-public class PlayerMixin {
-    @Inject(method = "getSkin", at = @At("HEAD"), cancellable = true)
-    private void onGetCapeLocation(CallbackInfoReturnable<ResourceLocation> cir) {
-        AbstractClientPlayer player = (AbstractClientPlayer) (Object) this;
-        ResourceLocation cape = CapeManager.INSTANCE.getPlayerCape(player.getGameProfile().getName());
-        if (cape != null && CapeManager.INSTANCE.isEnabled()) {
-            cir.setReturnValue(cape);
-            cir.cancel();
-        }
-    }
-}
+ @Mixin(AbstractClientPlayer.class)
+ public abstract class PlayerMixin {
+     @Shadow @Nullable private PlayerInfo playerInfo;
+
+     @Inject(method = "getSkin", at = @At("HEAD"), cancellable = true)
+     private void getCustomSkin(CallbackInfoReturnable<PlayerSkin> cir) {
+         if (playerInfo != null) {
+             String playerName = playerInfo.getProfile().getName();
+             ResourceLocation customSkinLocation = CapeManager.INSTANCE.getPlayerCape(playerName);
+             if (customSkinLocation != null) {
+                 PlayerSkin customSkin = new PlayerSkin(customSkinLocation, playerInfo.getSkin().textureUrl(), customSkinLocation, customSkinLocation, playerInfo.getSkin().model(), playerInfo.getSkin().secure());
+                 cir.setReturnValue(customSkin);
+             }
+         }
+     }
+ }
